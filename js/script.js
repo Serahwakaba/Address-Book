4e77faf8251dfed3d93b50a4e2613311d0cdf4e7
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-04-22 23:06:26 +0300

    add all changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    
-})
+function contact(first, last, email, phone,location) {
+    this.first = first;
+    this.last = last;
+    this.email = email;
+    this.location =[]
+}
+function Location(street, city, county){
+    this.street = street;
+     this.city = city;
+     this.state = state;
+}
+contact.prototype.fullName = function(){
+    return this.first + " " + this.last;
+}
+
+Location.prototype.address = function(){
+    return this.street + "," + this.city + "," +this.state;
+}
